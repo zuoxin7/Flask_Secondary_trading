e76caf464068a01e581a929e3e4fd7f9af3b4d80
zuoxin <zuoxin2008@126.com>
2017-11-29 10:15:47 +0800

    文件名保存到数据库

--- app/goodsissue/views.py
+++ app/goodsissue/views.py
@@ -12,16 +12,16 @@ def issue():
     form = GoodsForm()
     if form.validate_on_submit():
         lenid = GoodsissueGoods.select()
+        filename = secure_filename(form.image.data.filename)
+        form.image.data.save('app/static/upload/' + filename)
         good = GoodsissueGoods(id=len(lenid)+1,
                                owner=request.cookies.get('username',default='zhangjin'),
                                name=form.name.data,
                                introduction=form.introduction.data,
                                category=form.category.data,
-                               price=form.price.data)
+                               price=form.price.data,
+                               imagefile=filename)
         good.save()
-        filename = secure_filename(form.image.data.filename)
-        print(filename)
-        form.image.data.save('app/static/upload/' + filename)
         flash('你已经发布成功')
         return redirect(url_for('main.index'))
     return render_template('add_good.html', form=form)

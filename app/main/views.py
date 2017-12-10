a909411a00e6dcc429ea600339d4bba0daffc96d
tudoudou <ctudoudou@foxmail.com>
2017-12-10 13:02:43 +0800

    修正首頁，修正錯誤

--- app/main/views.py
+++ app/main/views.py
@@ -4,11 +4,16 @@ from flask_login import login_required, current_user
 from . import main
 from ..models import User, Post
 from .. import db
+from ..goodsissue.views import GoodsissueGoods
+from ..bounty.views import Bountyissue
+
 
 @main.route('/', methods=['GET', 'POST'])
 def index():
     rank = User.select().order_by(User.num.desc())
-    return render_template('index.html', rank=rank)
+    goods = GoodsissueGoods.select().where(GoodsissueGoods.good_trade == False).limit(3)
+    bountys = Bountyissue.select().where(Bountyissue.bounty_trade == False).limit(3)
+    return render_template('index.html', rank=rank, goods=goods,bountys=bountys)
 
 
 @main.route('/message', methods=['GET', 'POST'])
@@ -21,4 +26,3 @@ def message():
         return redirect(url_for('.message'))
     posts = Post.select().order_by(Post.timestamp.desc())
     return render_template('messagepush.html', form=form, posts=posts)
-

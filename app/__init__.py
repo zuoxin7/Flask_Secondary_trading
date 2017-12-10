c6c44f2b573d568b8f630691b4eb53c9aa5d4bc5
tudoudou <ctudoudou@foxmail.com>
2017-12-10 11:56:47 +0800

    添加并修正页脚

--- app/__init__.py
+++ app/__init__.py
@@ -21,6 +21,7 @@ def create_app(config_name):
     app = Flask(__name__)
     app.config.from_object(config[config_name])
     config[config_name].init_app(app)
+    app.config['BOOTSTRAP_SERVE_LOCAL'] = True
 
     bootstrap.init_app(app)
     moment.init_app(app)
